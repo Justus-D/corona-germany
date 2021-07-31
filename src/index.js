35c934cc17d45412516c5dfd53a1370bb7897b33
Justus <37590567+Justus-D@users.noreply.github.com>
2021-07-31 02:29:00 +0200

    Added console.log(); message

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ if (window.location.pathname.substr(-1,1) !== "/") {
   window.location.pathname = window.location.pathname+'/';
 }
 
+console.log("%cCorona 7-Tage-Inzidenz der letzten Tage", "color: #000; background-color: black; padding: 20px;");
+console.log("%cCorona 7-Tage-Inzidenz der letzten Tage", "color: #0f0; background-color: black; padding: 20px;");
+console.log("%cCorona 7-Tage-Inzidenz der letzten Tage", "color: #000; background-color: black; padding: 20px;");
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
