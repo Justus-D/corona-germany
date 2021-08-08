893026cdc3776db4d4391b180a1163933a8c778e
Justus <37590567+Justus-D@users.noreply.github.com>
2021-08-08 23:43:26 +0200

    CSS improvements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -11,8 +11,10 @@ type HeaderProps = {
 export default function Header(props: HeaderProps) {
 	return (
 		<div>
-			{props.hideStart ? null : <Link to="/" className="list-button start">Start</Link>}
-			{props.hideTop ? null : <Link to="/top" className="list-button top">Top Inzidenzen</Link>}
+			<div className="nav-links">
+				{props.hideStart ? null : <Link to="/" className="list-button start">Start</Link>}
+				{props.hideTop ? null : <Link to="/top" className="list-button top">Top Inzidenzen</Link>}
+			</div>
 			<div className="heading">{props.title}</div>
 			{props.subtitle ? <div className="description">{props.subtitle}</div> : null}
 		</div>
