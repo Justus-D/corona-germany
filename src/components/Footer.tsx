c980914be3b0bd8b1abc74673f353430d9979ea5
Justus <37590567+Justus-D@users.noreply.github.com>
2021-12-27 22:02:47 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,6 +5,7 @@ const commitSHA = process.env.CF_PAGES_COMMIT_SHA || "0123456789abcdef0123456789
 const branch = process.env.CF_PAGES_BRANCH || "dev"
 
 export default function Footer() {
+	console.log(process.env)
 	return (
 		<footer style={{padding: "10px"}}>
 			<span>Datenquelle 7-Tage-Inzidenzen: Robert Koch-Institut (siehe GitHub: <a className="footer" href="https://github.com/marlon360/rki-covid-api#data-sources" target="_BLANK" rel="noreferrer">rki-covid-api</a>)</span>
@@ -19,7 +20,7 @@ export default function Footer() {
 			{packageJson.version ?
 				<>
 					<br /><br />
-					<span>Version: v{packageJson.version}{pages ? <>&nbsp;{branch}&nbsp;<span title={commitSHA}>{commitSHA.substring(0,7)}</span></> : null}</span>
+					<span>Version: v{packageJson.version}{pages||true ? <>&nbsp;{branch}&nbsp;<span title={commitSHA}>{commitSHA.substring(0,7)}</span></> : null}</span>
 				</>
 			: null }
 		</footer>
